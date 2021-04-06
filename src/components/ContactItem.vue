3fede619a8f29005287ffd1a7cebba0464f5b61c
leandro47 <leandrosilva47@live.com>
2021-04-05 22:36:20 -0300

    contact is done

---
+++ src/components/ContactItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="col-sm-12 col-md-4 mt-4">
+    <div class="card text-center" style="width: 100%">
+      <a :href="link" target="_blank">
+        <div class="card-body">
+          <h5 class="card-title">{{ netWork }}</h5>
+          <span style="font-size: 4em">
+            <i :class="icon"></i>
+          </span>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    netWork: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+a{
+ text-decoration: none;
+ color: black;
+}
+a:hover{
+    color: white;
+    background-color: black;
+}
+</style>
--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -2,78 +2,30 @@
   <section class="page-section" id="contact">
     <div class="container">
       <div class="text-center">
-        <h2 class="section-heading text-uppercase">Contact Us</h2>
+        <h2 class="section-heading text-uppercase">Contact</h2>
         <h3 class="section-subheading text-muted">
-          Lorem ipsum dolor sit amet consectetur.
+          Some places you can find me. ;)
         </h3>
       </div>
-      <form id="contactForm" name="sentMessage" novalidate="novalidate">
-        <div class="row align-items-stretch mb-5">
-          <div class="col-md-6">
-            <div class="form-group">
-              <input
-                class="form-control"
-                id="name"
-                type="text"
-                placeholder="Your Name *"
-                required="required"
-                data-validation-required-message="Please enter your name."
-              />
-              <p class="help-block text-danger"></p>
-            </div>
-            <div class="form-group">
-              <input
-                class="form-control"
-                id="email"
-                type="email"
-                placeholder="Your Email *"
-                required="required"
-                data-validation-required-message="Please enter your email address."
-              />
-              <p class="help-block text-danger"></p>
-            </div>
-            <div class="form-group mb-md-0">
-              <input
-                class="form-control"
-                id="phone"
-                type="tel"
-                placeholder="Your Phone *"
-                required="required"
-                data-validation-required-message="Please enter your phone number."
-              />
-              <p class="help-block text-danger"></p>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="form-group form-group-textarea mb-md-0">
-              <textarea
-                class="form-control"
-                id="message"
-                placeholder="Your Message *"
-                required="required"
-                data-validation-required-message="Please enter a message."
-              ></textarea>
-              <p class="help-block text-danger"></p>
-            </div>
-          </div>
-        </div>
-        <div class="text-center">
-          <div id="success"></div>
-          <button
-            class="btn btn-primary btn-xl text-uppercase"
-            id="sendMessageButton"
-            type="submit"
-          >
-            Send Message
-          </button>
-        </div>
-      </form>
+    <div class="row">
+      <Network :net-work="'Twitter'" :icon="'fab fa-twitter-square'" :link="'https://twitter.com/LeandroDS47'" />
+      <Network :net-work="'Facebook'" :icon="'fab fa-facebook-square'" :link="'https://www.facebook.com/leandro.silva.5059601/'" />
+      <Network :net-work="'Linkedin'" :icon="'fab fa-linkedin'" :link="'https://www.linkedin.com/in/leandro-silva-a5b84318b/'" />
+      <Network :net-work="'Github'" :icon="'fab fa-github-square'" :link="'https://github.com/leandro47'" />
+      <Network :net-work="'Email'" :icon="'fas fa-envelope'" :link="'mailto:leandrosilva47@live.com'" />
+      <Network :net-work="'WhatsApp'" :icon="'fab fa-whatsapp-square'" :link="'https://api.whatsapp.com/send?phone=5547988387536'" />
+    </div>
     </div>
   </section>
 </template>
 
 <script>
-export default {};
+import Network from './../components/ContactItem'
+export default {
+  components: {
+    Network
+  }
+};
 </script>
 
 <style scoped>
