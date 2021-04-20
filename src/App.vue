935491ae5f635057534058b565a23eb17975c38d
leandro47 <leandrosilva47@live.com>
2021-04-19 21:27:01 -0300

    created login in the api

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <Menu />
     <div class="mt-4"></div>
     <transition name="fade-view" mode="out-in">
-        <router-view></router-view>
+      <router-view></router-view>
     </transition>
     <Footer />
   </div>
@@ -17,17 +17,35 @@ export default {
   name: "App",
   components: {
     Menu,
-    Footer
+    Footer,
+  },
+  methods: {
+    auth() {
+      fetch("http://localhost:8080/auth", {
+        method: "POST",
+        body: JSON.stringify({
+          user: "LEANDRO DA SILVA",
+          pwd: "minhaSenha",
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => localStorage.setItem("Token_Api", data.data))
+        .catch((error) => console.log(error));
+    },
+  },
+  created() {
+    this.auth();
   },
 };
 </script>
 
 <style scoped>
-
-.fade-view-enter, .fade-view-leave-to {
+.fade-view-enter,
+.fade-view-leave-to {
   opacity: 0;
 }
-.fade-view-enter-active, .fade-view-leave-active {
-  transition: opacity .5s ease-in-out;
+.fade-view-enter-active,
+.fade-view-leave-active {
+  transition: opacity 0.5s ease-in-out;
 }
 </style>
--- src/store/modules/tecnologies.js
+++ src/store/modules/tecnologies.js
@@ -18,10 +18,14 @@ export default {
         setSkills(context) {
             fetch(URL_API, {
                 method: 'GET',
+                headers: new Headers({ Authorization: `Basic ${localStorage.getItem('Token_Api')}` })
             })
             .then((response) => response.json())
             .then((data) => context.commit("setSkills", data.data))
-            .catch((error) => console.log(error))
+            .catch((error) => {
+                console.log(error);
+                console.log(`Basic ${localStorage.getItem('Token_Api')}`);
+            })
         }
     }
 }
